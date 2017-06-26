ddbac43fb655955c5f8386cbe1765c31269d3888
Nicholas <nicholas.mueller@gmail.com>
2017-06-26 17:24:24 -0600

    inital2

--- src/components/Timeline/timeline.js
+++ src/components/Timeline/timeline.js
@@ -1,52 +1,143 @@
 import React, { Component } from 'react';
+import $ from 'jquery'
 import './timeline.css';
 
 export default class Timeline extends Component {
 
+
 render() {
+
     return (
 
-<section className="timeline-body">
+<div className="timeline-body">
+  <div className="header"></div>
+<section id="cd-timeline" className="cd-container">
+		<div className="cd-timeline-block">
+			<div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+    
 
-<div className="timeline-container timeline-theme-1">
-  <div className="timeline js-timeline">
-    <div data-time="1839">Soda was invented.</div>
-    <div data-time="1862">United States Department of Agriculture(USDA) was created by President Abhraham Lincoln.</div>
-    <div data-time="1892">Coca-Cola Company was founded.</div>
-    <div data-time="1893">United States Department of Agriculture(USDA)begins funding research on nutrition. </div>
-    <div data-time="1898">Pepsi was founded.</div>
-    <div data-time="1900">The average American ate 4 pounds of Cheese. In the year 2000, the figure increased to up to 8 pounds per year.</div>
-    <div data-time="1906">The United States congress passed the Pure Food and Drug Act. Which helped regulate the meatpacking and medicine industries.</div>
-    <div data-time="1906">The Food and Drug Administration(FDA) was founded.</div>
-    <div data-time="1916">The Piggly Wiggly Chain of Grocery Stores was founded in Memphis,TN. Piggly Wiggly  the first self service Grocery stores.</div>
-    <div data-time="1925">Lucky Strike Cigarette brand launches its  ad campaign "Reach for a Lucky instead of a Sweet", using nicotine's appetite supressing abilities to increase sales.</div>
-    <div data-time="1920s">USDA produced radio show titled "Aunt Sammy" which informed listeners on recipes and cooking tips. Also informed them on nutrion.</div>
-    <div data-time="1930s">During the Great Depression, focus on diets switched to getting enough daily calories.</div>
-    <div data-time="1943">The War Food Administration expands lunch program, making sure children are fed during the day.</div>
-    <div data-time="1946">Chick-fil-A was founded.</div>
-    <div data-time="1950s">University of Minnesota Dr. Ancel Keys, research finds that heart disease has a direct correlation with diet and exercise.</div>
-    <div data-time="1955">McDonald's was founded on April 15,1955.</div>
-    <div data-time="1960">President John F. Kennedy writes an article for Sports Illustrated urging Americn citizens to increase their daily fitness levels. Said the United States was under-exercised.</div>
-    <div data-time="1960">Domino's Pizz was founded. </div>
-    <div data-time="1962">Taco Bell was Founded on March 21,1962.</div>
-    <div data-time="1962">United States has 13% obesity rate. 45% of Americans were overweight.</div>
-    <div data-time="1963">Weight Watchers was founded.</div>
-    <div data-time="1968">Big Mac was introduced at McDonalds</div>
-    <div data-time="1971">StarBucks was founded.</div>
-    <div data-time="1972">Dr.Atkin's publishes first diet.</div>
-    <div data-time="1980s">The start of America's Obesity epidemic begins</div>
-    <div data-time="1981">The USDA announced that ketchup could be counted as a vegetable in the school lunch program.</div>
-    <div data-time="1981">The Food and Drug Administration(FDA) approved the artifical sweetener Aspartame for tabletop use.</div>
-    <div data-time="1992">The USDA publishes the food pyramid.</div>
-    <div data-time="1994">Guide to Nutrition Labeling and Education Act requires food companies to include nutritional info on packaging.</div>
-    <div data-time="1995">United States consumption of fast food establisments triples.</div>
-    <div data-time="2004">U.S Obesity rate reached critical levels at 45%</div>
-    <div data-time="2005">USDA replaces the Food Pyramid with MyPyramid.</div>
-    <div data-time="2008">Estimated annual medical cost of obesity in the United States was over $147 billion. Medical Costs for obese people were on average $1,429 times higher than those of normal weight.</div>
-    <div data-time="2011">USDA replaces the MyPyramid with MyPlate/MiPlato</div>
-  </div>
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+      <div className="cd-timeline-content">
+				<h2>Title of section 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+				<span className="cd-date">Jan 14</span>
+			</div>
+		</div>
+
+		<div className="cd-timeline-block">
+			
+
+			</div>
+	</section>
+
 </div>
-
-</section>
-
 )}}
