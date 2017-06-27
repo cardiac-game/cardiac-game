5ba349a0da56e757a61620b39b6ecd2ad44bf025
Andi Anderson <nephaelin@Andis-MacBook-Pro.local>
2017-06-26 18:04:59 -0600

    FrontEnd-InputPage

--- src/components/HealthInput/healthInput.js
+++ src/components/HealthInput/healthInput.js
@@ -127,19 +127,20 @@ class HealthInput extends Component {
         // display the list of all foods return by the user search
         const listOfFoods = this.state.allFoodsArray.map((item => {
             return (
-                <div key={item.ndbno} onClick={() => this.addToUserList(item.ndbno)}>{item.name}</div>
+                <div className="list-border" key={item.ndbno} onClick={() => this.addToUserList(item.ndbno)}>{item.name}</div>
             )
         }))
 
         // display the list of all the foods the user has clicked on/ collected
         const listOfUserFoods = this.state.chosenFoodsArray.map((item, i) => {
             return (
-                <div key={i}>
+                <div className="list-border" key={i}>
                 <div>{item.name}</div>
-                <span>Serving Size: {item.nutrients[0].measures[0].qty} {item.nutrients[0].measures[0].label}</span>
-                <span>Servings:</span>
-                <input placeholder={this.state.chosenFoodsArray[i].qty} onChange={e => this.updateServings(i, e.target.value)}/>
-                <button onClick={() => this.removeUserFood(i)}>X</button>
+                <span> Servings: </span>
+                <input  className="input-size" placeholder={this.state.chosenFoodsArray[i].qty} onChange={e => this.updateServings(i, e.target.value)}/>
+                <span> (Serving Size: {item.nutrients[0].measures[0].qty} {item.nutrients[0].measures[0].label})</span>
+                
+                <button className="remove-button" onClick={() => this.removeUserFood(i)}>X</button>
                 </div>
             )
         })
@@ -156,12 +157,65 @@ class HealthInput extends Component {
 
         return (
         <div className="input-input">
-            <div className="nutrition-info">{ nutritionInfo }</div>       
-            <div className="selected-info">{ listOfUserFoods }</div>
-            <input className="input-box" value={this.state.userInput} onChange={e => this.setState({userInput: e.target.value})}></input>
-            <button className="input-button-1" disabled={this.state.userInput ? false : true} onClick={() => this.getFoodsArray(this.state.userInput)}>Search</button>
-            <button className="input-button-2" disabled={this.state.chosenFoodsArray.length === 0 ? true : false}>Submit</button>
-            <div className="food-list">{ listOfFoods }</div>
+            <div className="input-intro">
+                <div className="input-curtain"></div>
+            </div>
+            
+            <div className="input-boxy-0">
+            
+                <div className="input-boxy-1">
+                    <div className="input-boxy-11">
+                        <input className="input-bar" value={this.state.userInput} onChange={e => this.setState({userInput: e.target.value})}></input>
+                        <button className="input-button-1" disabled={this.state.userInput ? false : true} onClick={() => this.getFoodsArray(this.state.userInput)}>SEARCH</button>
+                        </div>
+                    
+                    <div className="input-list">
+                    <div className="food-list">{ listOfFoods }</div>
+                </div></div>   
+
+              {
+              
+                    listOfUserFoods.length
+                    ?
+                <div className="input-boxy-2">
+            
+                    <div className="input-boxy-21">
+                        <div className="selected-info">{ listOfUserFoods }</div>
+                    </div>
+
+                    <div className="input-boxy-22">
+                        <div className="nutrition-info">{ nutritionInfo }</div>
+                           </div>
+                           <div className="input-boxy-maps">
+                               <div className="game-map-title">GAME MAP</div>
+                            <form action="">
+                               <input class="input-radio" type="radio" name="map"/> CANCEROUS<br/>
+                               <input class="input-radio" type="radio" name="map"/> GALACTIC<br/>
+                               <input class="input-radio" type="radio" name="map"/> HEART ATTACK<br/>
+                               <input class="input-radio" type="radio" name="map"/> INFECTION<br/>
+                               <input class="input-radio" type="radio" name="map"/> MICROSCOPIC<br/>
+                               <input class="input-radio" type="radio" name="map"/> PARASITICAL<br/>
+                            </form>
+                           </div>
+                    </div>
+                    :
+                    <div className="first-box">
+                        <div className="input-curtain-first">
+                            <div className="first-title">NDC (NUTRITION DATA CALCULATOR)</div>
+                            <div className="first-text">Our NDC can take in almost any food products from around the globe, extract the nutritional data from these products and calculate the data in a simple and presentible manner.</div>
+                        </div>
+                         
+                          
+                    </div>
+
+                }        
+
+            </div>
+
+                    <div className="input-submit">
+                    <button className="input-button-2" disabled={this.state.chosenFoodsArray.length === 0 ? true : false}>SUBMIT</button>
+                    </div>
+                  
         </div>
         )
 
@@ -177,5 +231,4 @@ function mapStateToProps(state){
 
 export default connect(mapStateToProps)(HealthInput);
 
-/// get rid of upc
-// filter out duplicates
+//fix break when invalid input.
