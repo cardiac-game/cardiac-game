ce55c86c1123e0234e0062ad889f7cbadd1a0bd8
Justice White <jaxingermany@Justice-Whites-MacBook-Pro.local>
2017-07-06 19:09:35 -0600

    fixed error

--- src/store/ducks/nutritionReducer.js
+++ src/store/ducks/nutritionReducer.js
@@ -1,5 +1,25 @@
 const initialState = {
-  inputsObj: {}
+  inputsObj: {
+            playerInitialSpeed: 1,
+            playerOverallSpeed: 1,
+            playerFireRate: 1,
+            playerMaxShield: 1,
+
+            bulletBonusDamage: 1,
+
+            // THESE ARE ADDITIONAL SPAWNED ENEMIES - can never be lower than 0 (range from 0 to 1)
+            sugarAmount: 1,
+            cholesterolAmount: 1,
+
+            enemySpeed: 1,
+            maxOnScreenEnemies: 1,
+
+            powerupHeartHealth: 0, // 0 for powerups is the bare minimum (0 powerups of this type should spawn)
+            powerupFirePower: 0,
+            powerupFireRate: 0,
+            powerupCholesterolBomb: 0,
+            powerupPlayerMaxShield: 0,
+        }
 }
 
 const POST_GAME_INPUT = "POST_GAME_INPUT";
